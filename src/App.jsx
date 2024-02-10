411746a4a0b09ee8756daa255cbf73c229b0e4b9
evercode1 <ikon321@yahoo.com>
2024-02-09 16:12:32 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    <div className="text-center">
+
+        <h1>Hello Me</h1>
+
+    </div>
+      
+    </>
+  )
+}
+
+export default App
